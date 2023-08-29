812008bef191b898957a1ffd4df3503a8b806e08
williamkemeny <83621324+williamkemeny@users.noreply.github.com>
2023-08-29 19:54:05 -0400

    Update TopNavigationBar.jsx
    search bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,10 +4,18 @@ import "../styles/TopNavigationBar.scss";
 import FavBadge from "./FavBadge";
 
 const TopNavigation = (props) => {
+  const inputChange = (event) => props.handleChange(event.target.value);
+  const selectChange = (event) => props.handleChange2(event.target.value);
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <div className="top-nav-bar">
+        <select onChange={selectChange}>
+          <option>City</option>
+          <option>Country</option>
+          <option>Username</option>
+        </select>
+        <input onChange={inputChange}></input>
         <TopicList
           topics={props.topics}
           onTopicSelect={props.onTopicSelect}
